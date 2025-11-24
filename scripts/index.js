8b4e754bc6f7bf046bbf7a521e5553ee6c05718a
Deepesh70 <deepeshdangi700@gmail.com>
2025-11-24 22:45:29 +0530

    task management UI rendering and state.

--- scripts/index.js
+++ scripts/index.js
@@ -18,9 +18,39 @@ const htmlTaskContent = ({ id, title, description, tags, imageUrl }) => `
     </button>
 
     </div>
+    <div class="card-body">
+    ${url && `<img width="100%" src=${url} alt="card image cap class="card-image-top md-3 rounded-lg"/> `} 
+    <h4 class="task_card_title">${title}</h4    >
+    <p class="task_card_description">${description}</p>
+    <p class="task_card_tags">${tags}</p>
+    </div> 
 
+    <div class="card-footer">
+    <button
+        type="button"
+        class="btn btn-outline-primary float-right"
+        data-bs-toggle="modal"
+        data-bs-target="#taskModal"
+
+    >Open Task
+    </button>
+    </div>
     </div>
 
     </div>
 
-`
+`;
+
+
+const htmlModalContent = ({id, title, description, url, tags}) => {
+    const date = new Date(parseInt(id));
+    return `
+    <div id=${id}>
+         ${url && `<img width="100%" src=${url} alt="card image cap class="img-fluid place_holder_image mb-3"/> `} 
+         <strong class="text-sm text-muted">Created on ${date.toDateString()}</strong>
+         <h3 class="my-3">${title}</h3>
+         <p class="lead">${description}</p>
+
+    </div>
+    `;
+}
